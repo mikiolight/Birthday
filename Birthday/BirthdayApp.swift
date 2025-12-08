13b34848419fc8f72cf201a961289f0ccec962aa
mikio <43379090+mikiolight@users.noreply.github.com>
2025-12-08 23:42:54 +0900

    Initial Commit

---
+++ Birthday/BirthdayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayApp.swift
+//  Birthday
+//
+//  Created by Mikihisa Saito on 2025/12/08.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Birthday/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Birthday
+//
+//  Created by Mikihisa Saito on 2025/12/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
